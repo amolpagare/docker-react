a8d14eb4127d54768593bbda390a9fabf5249f1c
Amol N. Pagare <APagare@CAPMPTDC.COM>
2019-09-28 18:10:56 +0530

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         I love My India!
+        <h1 className="App-title">Welcome To React</h1>
+        <p className="App-intro">
+         I was changed On the Feature branch
         </p>
         <a
           className="App-link"
